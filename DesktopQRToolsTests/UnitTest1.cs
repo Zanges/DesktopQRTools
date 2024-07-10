3319c8d7a8a79300cb14fe0a152a01b328a7f0e2
Zanges <3868054+Zanges@users.noreply.github.com>
2024-07-10 21:23:58 +0200

    added DesktopQRToolsTests Project

---
+++ DesktopQRToolsTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace DesktopQRToolsTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
