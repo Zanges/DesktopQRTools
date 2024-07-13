1205b76087847001de3b131dbe8184d4c8cc2a42
Zanges <3868054+Zanges@users.noreply.github.com>
2024-07-13 02:13:55 +0200

    Added a new unit test for the QRCodeGeneratorWindow class to verify the auto-save file name generation functionality.

--- DesktopQRToolsTests/MiscTests.cs
+++ DesktopQRToolsTests/MiscTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.IO.Abstractions.TestingHelpers;
 using DesktopQRTools;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -41,6 +42,32 @@ namespace DesktopQRToolsTests
 
             // Clean up
             File.Delete(testConfigPath);
+        }
+
+        [TestMethod]
+        public void TestQRCodeGeneratorWindowAutoSaveFileName()
+        {
+            // Arrange
+            var mockFileSystem = new MockFileSystem();
+            var configPath = "C:\\TestConfig\\config.ini";
+            mockFileSystem.AddFile(configPath, new MockFileData(
+                "AutoSaveQRCodeName=TestQR\n" +
+                "SkipSaveDialog=true\n" +
+                "AutoSaveDirectory=C:\\TestSaveDir\n" +
+                "AppendDate=true\n" +
+                "AppendTime=true\n"
+            ));
+
+            var window = new QRCodeGeneratorWindow(configPath, mockFileSystem);
+
+            // Act
+            string fileName = window.GetAutoSaveFileName();
+
+            // Assert
+            Assert.IsTrue(fileName.StartsWith("TestQR-"), "File name should start with TestQR-");
+            Assert.IsTrue(fileName.EndsWith(".png"), "File name should end with .png");
+            Assert.IsTrue(fileName.Contains(DateTime.Now.ToString("yyyyMMdd")), "File name should contain the current date");
+            Assert.IsTrue(fileName.Contains(DateTime.Now.ToString("HHmmss")), "File name should contain the current time");
         }
     }
 }
